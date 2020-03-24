df8ebc24c9ab56143849d375136acbbe5d6b5f95
Akshay Patel <akkipatel667@gmail.com>
2020-03-23 22:19:36 -0400

    bit manipulation using bitwise operators

---
+++ 371_Sum_of_Two_Integers_easy.py
@@ -0,0 +1,28 @@
+"""
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = -2, b = 3
+Output: 1
+
+"""
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # 32 bit mask in hexadecimal
+        mask = 0xffffffff
+        
+        # works both as while loop and single value check 
+        while (b & mask) > 0:
+            
+            carry = ( a & b ) << 1
+            a = (a ^ b) 
+            b = carry
+        
+        # handles overflow
+        return (a & mask) if b > 0 else a
